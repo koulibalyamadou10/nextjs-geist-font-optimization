26ff162c033aa8285b40cab5c649725306dd0123
Koulibaly <koulibalyamadou10@gmail.com>
2025-06-03 13:18:46 +0000

    Add src/app/certifications/page.tsx 1748956704910

---
+++ src/app/certifications/page.tsx
@@ -0,0 +1,21 @@
+export default function Certifications() {
+    return (
+        <section id="education" className="w-11/12 max-w-4xl my-12">
+            <h2 className="text-3xl font-bold mb-4">Education</h2>
+            <ul>
+                <li>
+                    <strong>React JS</strong> (18 au 05 Avril 2024) - Orange Digital Center
+                </li>
+                <li>
+                    <strong>Fondamentaux du système Linux</strong> (01 au 3 Août 2023) - Orange Digital Center
+                </li>
+                <li>
+                    <strong>Développement Fullstack</strong> (8 au 19 Août 2023) - Orange Digital Center
+                </li>
+                <li>
+                    <strong>Java Spring Boot</strong> (01 au 12 Juillet 2024) - Orange Digital Center
+                </li>
+            </ul>
+        </section>
+    )
+}
