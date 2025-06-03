63fbc8feaa6b87511333d7b39012e68802158621
Koulibaly <koulibalyamadou10@gmail.com>
2025-06-03 13:18:43 +0000

    Add src/app/experiences/page.tsx 1748956702393

---
+++ src/app/experiences/page.tsx
@@ -0,0 +1,24 @@
+export default function Experiences() {
+  return (
+      <section id="experience" className="w-11/12 max-w-4xl my-12">
+          <h2 className="text-3xl font-bold mb-4">Experience</h2>
+          <div>
+              <h3 className="font-semibold">Système de gestion decole (2023 - 2024)</h3>
+              <p>
+                  Développement dun système de gestion decode en Java permettant
+                  inscriber, de payer la mensualité et destroyer des notifications
+                  aux parents.
+              </p>
+          </div>
+          <div className="mt-4">
+              <h3 className="font-semibold">
+                  Développeur dans la team Kakoulima - Orange Summer Challenge 2024
+              </h3>
+              <p>
+                  Conception dun dispositif révolutionnaire pour agriculture,
+                  permettant de surveiller et protéger les champs.
+              </p>
+          </div>
+      </section>
+  );
+}
